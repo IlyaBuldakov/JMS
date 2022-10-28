70eefbdee20bb30bbeb785d661d2a1bf2b3f81d4
Buldakov Ilya <jdoe@example.com>
2022-10-28 15:19:05 +0400

    Fixed MessageService class

--- producer/src/main/java/model/service/MessageService.java
+++ producer/src/main/java/model/service/MessageService.java
@@ -3,24 +3,40 @@ package model.service;
 import model.MetricProducer;
 import model.hardware.HardwareAnalyser;
 import model.hardware.Metrics;
+import model.hardware.impl.CpuHardwareAnalyserImpl;
+import model.hardware.impl.DiskHardwareAnalyserImpl;
+import model.hardware.impl.RamHardwareAnalyserImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class MessageService {
 
     private static final Logger LOG = LoggerFactory.getLogger(MessageService.class);
 
-    HardwareAnalyser hardwareAnalyser;
+    private final HardwareAnalyser[] hardwareAnalysers = {
+            new CpuHardwareAnalyserImpl(),
+            new DiskHardwareAnalyserImpl(),
+            new RamHardwareAnalyserImpl()
+    };
 
-    MetricProducer metricProducer;
+    private final MetricProducer metricProducer;
+
+    public MessageService(MetricProducer metricProducer) {
+        this.metricProducer = metricProducer;
+    }
 
     public void proceed() {
         while (true) {
             delay(1000);
-            Map<Metrics, Object> analysedInfo = hardwareAnalyser.analyse();
+            Set<Map<Metrics, Object>> analysedInfo = new HashSet<>();
+            for (HardwareAnalyser analyser : hardwareAnalysers) {
+                analysedInfo.add(analyser.analyse());
+            }
             metricProducer.send(analysedInfo);
         }
     }
