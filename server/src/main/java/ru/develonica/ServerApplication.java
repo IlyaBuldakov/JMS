c07370deccce13993f4585fa02fb94546186ed91
Buldakov Ilya <jdoe@example.com>
2022-10-28 12:16:12 +0400

    Added ServerApplication main class

---
+++ server/src/main/java/ru/develonica/ServerApplication.java
@@ -0,0 +1,32 @@
+package ru.develonica;
+
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.develonica.model.YamlParser;
+
+import java.net.URI;
+
+public class ServerApplication {
+
+    private static final String BROKER_ADDRESS_YAML_KEY = "address";
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServerApplication.class);
+
+    public static void main(String[] args) {
+        try {
+            YamlParser yamlParser = new YamlParser();
+            String address = yamlParser.getValueFromProperties(String.class, BROKER_ADDRESS_YAML_KEY);
+            BrokerService broker = BrokerFactory.createBroker(new URI(
+                    "broker:(tcp://" + address + ")"));
+            broker.start();
+            Object lock = new Object();
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (Exception exception) {
+            LOG.error(exception.toString());
+        }
+    }
+}
