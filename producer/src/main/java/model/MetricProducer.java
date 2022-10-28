ec741f5e859db84a621fc23a2e57d557657d3479
Buldakov Ilya <jdoe@example.com>
2022-10-28 15:18:32 +0400

    MetricProducer class completed

--- producer/src/main/java/model/MetricProducer.java
+++ producer/src/main/java/model/MetricProducer.java
@@ -1,12 +1,49 @@
 package model;
 
 import model.hardware.Metrics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.jms.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.ZonedDateTime;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class MetricProducer {
 
-    public void send(Map<Metrics, Object> metrics) {
+    private static final Logger LOG = LoggerFactory.getLogger(MetricProducer.class);
 
+    private final BrokerEnvironmentHolder brokerEnv = new BrokerEnvironmentHolder();
+
+    public void send(Set<Map<Metrics, Object>> metrics) {
+        try {
+            BrokerMessage brokerMessage = new BrokerMessage(
+                    UUID.randomUUID(),
+                    ZonedDateTime.now(),
+                    metrics
+            );
+
+            Session activeSession = brokerEnv.getSession();
+            Topic topic = brokerEnv.getTopic();
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(buffer);
+            oos.writeObject(brokerMessage);
+            byte[] brokerMessageBytes = buffer.toByteArray();
+
+            BytesMessage message = activeSession.createBytesMessage();
+            message.writeBytes(brokerMessageBytes);
+            MessageProducer producer = activeSession.createProducer(topic);
+            producer.send(message);
+
+            buffer.close();
+            oos.close();
+        } catch (JMSException | IOException exception) {
+            LOG.error(exception.getMessage());
+        }
     }
 }
