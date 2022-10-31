37a125f94c89229bbf3edda7aa202bf72bf27805
Buldakov Ilya <jdoe@example.com>
2022-10-31 17:07:02 +0400

    Added message models

---
+++ consumer/src/main/java/model/broker/BrokerMessage.java
@@ -0,0 +1,45 @@
+package model.broker;
+
+import model.hardware.Metrics;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+public class BrokerMessage implements Serializable {
+
+    private final UUID id;
+
+    private final ZonedDateTime dateTime;
+
+    private final Set<Map<Metrics, Object>> value;
+
+    public BrokerMessage(UUID id, ZonedDateTime dateTime, Set<Map<Metrics, Object>> value) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.value = value;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public ZonedDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public Set<Map<Metrics, Object>> getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "BrokerMessage{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ consumer/src/main/java/model/hardware/Metrics.java
@@ -0,0 +1,10 @@
+package model.hardware;
+
+public enum Metrics {
+
+    CPU_PERCENT_LOAD,
+
+    RAM_MB_LOAD,
+
+    DISK_MB_FREE_SPACE
+}
