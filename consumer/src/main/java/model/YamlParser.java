896d3b6bbe408f945099561b51594ab038b8ecb7
Buldakov Ilya <jdoe@example.com>
2022-10-31 16:36:24 +0400

    Added consumer's YamlParser

---
+++ consumer/src/main/java/model/YamlParser.java
@@ -0,0 +1,20 @@
+package model;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class YamlParser {
+
+    private static final String PATH_TO_PROPERTIES = "consumer/src/main/resources/properties.yaml";
+
+    public <T> T getValueFromProperties(Class<T> clazz, String key) throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream(PATH_TO_PROPERTIES);
+        Yaml yaml = new Yaml();
+        Map<String, T> data = yaml.load(inputStream);
+        return data.get(key);
+    }
+}
