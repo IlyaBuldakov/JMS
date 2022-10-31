5120533393f6838ef9ba1d7c8b3fb91ae1fc6c78
Buldakov Ilya <jdoe@example.com>
2022-10-31 15:15:40 +0400

    Fixed RAM load metrics

--- producer/src/main/java/model/hardware/impl/RamHardwareAnalyserImpl.java
+++ producer/src/main/java/model/hardware/impl/RamHardwareAnalyserImpl.java
@@ -3,7 +3,6 @@ package model.hardware.impl;
 import model.hardware.HardwareAnalyser;
 import model.hardware.Metrics;
 import model.hardware.SystemInfoHolder;
-import oshi.SystemInfo;
 import oshi.hardware.GlobalMemory;
 
 import java.util.Map;
@@ -13,8 +12,11 @@ public class RamHardwareAnalyserImpl implements HardwareAnalyser {
     private static final GlobalMemory GLOBAL_MEMORY = SystemInfoHolder.SYSTEM_INFO
             .getHardware().getMemory();
 
+    private static final int BYTES_IN_GB = 1_073_741_824;
+
     @Override
     public Map<Metrics, Object> analyse() {
-        return Map.of(Metrics.RAM_MB_LOAD, GLOBAL_MEMORY.getAvailable());
+        return Map.of(Metrics.RAM_MB_LOAD,
+                (GLOBAL_MEMORY.getTotal() - GLOBAL_MEMORY.getAvailable()) / BYTES_IN_GB);
     }
 }
