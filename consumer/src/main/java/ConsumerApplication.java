6882666d2b0488f45aa3cfe7ad65579b63790a52
Buldakov Ilya <jdoe@example.com>
2022-10-31 17:13:57 +0400

    Changes in ConsumerApplication (main class)

--- consumer/src/main/java/ConsumerApplication.java
+++ consumer/src/main/java/ConsumerApplication.java
@@ -1,9 +1,11 @@
 import model.broker.MessageReceiver;
+import model.service.MessageService;
 
 public class ConsumerApplication {
 
     public static void main(String[] args) {
         MessageReceiver messageReceiver = new MessageReceiver();
-        messageReceiver.receive();
+        MessageService messageService = new MessageService(messageReceiver);
+        messageService.proceed();
     }
 }
