8683308fdb57f34fc0b3cb6199e83e270ffeb602
Buldakov Ilya <jdoe@example.com>
2022-11-01 11:23:04 +0400

    Refactoring (toString() method)

--- producer/src/main/java/model/broker/BrokerMessage.java
+++ producer/src/main/java/model/broker/BrokerMessage.java
@@ -4,6 +4,8 @@ import model.hardware.Metrics;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -12,10 +14,10 @@ public record BrokerMessage(UUID id, ZonedDateTime dateTime, Set<Map<Metrics, Ob
 
     @Override
     public String toString() {
-        return "BrokerMessage{" +
-                "id=" + this.id +
-                ", dateTime=" + this.dateTime +
-                ", value=" + this.value +
-                '}';
+        return "BrokerMessage{ id = %s | dateTime = %s, value = %s"
+                .formatted(
+                        this.id,
+                        this.dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)),
+                        this.value);
     }
 }
