4804790662a694b80bc4c8605dc6c1a469abbd7e
Buldakov Ilya <jdoe@example.com>
2022-11-03 12:12:01 +0400

    Javadoc, refactoring #3

--- server/src/main/java/ru/develonica/ServerApplication.java
+++ server/src/main/java/ru/develonica/ServerApplication.java
@@ -7,18 +7,23 @@ import ru.develonica.view.ApplicationView;
 
 import java.net.URI;
 
+/**
+ * Main server class.
+ */
 public class ServerApplication {
 
     private static final String BROKER_ADDRESS_YAML_KEY = "address";
 
     private static final ApplicationView APPLICATION_VIEW = new ApplicationView();
 
+    private static final String PROTOCOL_PREFIX = "broker:(tcp://";
+
     public static void main(String[] args) {
         try {
             YamlParser yamlParser = new YamlParser();
             String address = yamlParser.getValueFromProperties(BROKER_ADDRESS_YAML_KEY);
             BrokerService broker = BrokerFactory.createBroker(new URI(
-                    "broker:(tcp://" + address + ")"));
+                    PROTOCOL_PREFIX + address + ")"));
             broker.start();
             Object lock = new Object();
             synchronized (lock) {
--- server/src/main/java/ru/develonica/view/ApplicationView.java
+++ server/src/main/java/ru/develonica/view/ApplicationView.java
@@ -3,25 +3,24 @@ package ru.develonica.view;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Main application view class.
+ */
 public class ApplicationView {
 
+    /**
+     * Logger instance.
+     */
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationView.class);
 
+    /**
+     * Exception handler.
+     *
+     * @param exception Exception input.
+     */
     public void handleException(Exception exception) {
         String text = "Exception: %s. %s".formatted(exception.toString(), exception.getMessage());
         System.err.println(text);
         LOG.error(text);
-    }
-
-    public void handleInfoLog(String message) {
-        if (LOG.isInfoEnabled()) {
-            LOG.info(message);
-        }
-    }
-
-    public void handleErrorLog(String message) {
-        if (LOG.isErrorEnabled()) {
-            LOG.error(message);
-        }
     }
 }
