0e4aeb665dce2bafd0426a6ffc88ac0c4823f9fe
IlyaBuldakov <ggpanguin@gmail.com>
2022-11-04 20:56:49 +0400

    Added specified logs directory creation

--- consumer/src/main/java/model/AlertLogWriter.java
+++ consumer/src/main/java/model/AlertLogWriter.java
@@ -4,20 +4,28 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.Map;
 import model.hardware.Metrics;
 
 public class AlertLogWriter {
 
-  private static final String DESTINATION = "log.txt";
+  private static final Path FOLDER_NAME = Path.of("logs");
+
+  private static final String FILE_NAME = "alerts";
+
+  private static final String FILE_EXTENSION = ".txt";
 
   private static BufferedWriter writer;
 
   public void write(Map.Entry<Metrics, Object> entry) throws IOException {
     if (writer == null) {
-      new File(DESTINATION);
-      writer = new BufferedWriter(new FileWriter(DESTINATION, true));
+      Files.createDirectory(FOLDER_NAME);
+      String fullPath = FOLDER_NAME + "/" + FILE_NAME + FILE_EXTENSION;
+      new File(fullPath);
+      writer = new BufferedWriter(new FileWriter(fullPath, true));
     }
     writer.write("%s | %s".formatted(entry.toString(), LocalDateTime.now())
             + System.lineSeparator());
