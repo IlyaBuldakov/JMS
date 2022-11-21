442c41278cb9cfc6432fb845781bbab75b35a6ad
Buldakov Ilya <jdoe@example.com>
2022-11-21 18:00:07 +0400

    Added try-with-resources

--- producer/src/main/java/ru/develonica/model/broker/MetricProducer.java
+++ producer/src/main/java/ru/develonica/model/broker/MetricProducer.java
@@ -1,18 +1,18 @@
 package ru.develonica.model.broker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.develonica.common.model.hardware.Metrics;
+
+import javax.jms.BytesMessage;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.Topic;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
 import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.UUID;
-import javax.jms.BytesMessage;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.Topic;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.develonica.common.model.hardware.Metrics;
 
 /**
  * Class that creates {@link BrokerMessage}, translates
@@ -20,41 +20,39 @@ import ru.develonica.common.model.hardware.Metrics;
  */
 public class MetricProducer {
 
-  private static final Logger LOG = LoggerFactory.getLogger(MetricProducer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MetricProducer.class);
 
-  private final BrokerEnvironmentHolder brokerEnv;
+    private final BrokerEnvironmentHolder brokerEnv;
 
-  public MetricProducer(BrokerEnvironmentHolder brokerEnv) {
-    this.brokerEnv = brokerEnv;
-  }
+    public MetricProducer(BrokerEnvironmentHolder brokerEnv) {
+        this.brokerEnv = brokerEnv;
+    }
 
-  /**
-   * Send method.
-   *
-   * @param metrics Metric info (list of pairs).
-   */
-  public void send(HashMap<Metrics, Object> metrics) throws Exception {
-    BrokerMessage brokerMessage = new BrokerMessage(
-            UUID.randomUUID(),
-            ZonedDateTime.now(),
-            metrics
-    );
+    /**
+     * Send method.
+     *
+     * @param metrics Metric info (list of pairs).
+     */
+    public void send(HashMap<Metrics, Object> metrics) throws Exception {
+        BrokerMessage brokerMessage = new BrokerMessage(
+                UUID.randomUUID(),
+                ZonedDateTime.now(),
+                metrics
+        );
 
-    Session activeSession = this.brokerEnv.getSession();
-    Topic topic = this.brokerEnv.getTopic();
+        Session activeSession = this.brokerEnv.getSession();
+        Topic topic = this.brokerEnv.getTopic();
 
-    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-    ObjectOutputStream oos = new ObjectOutputStream(buffer);
-    oos.writeObject(brokerMessage);
-    byte[] brokerMessageBytes = buffer.toByteArray();
+        try (ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(buffer)) {
+            oos.writeObject(brokerMessage);
+            byte[] brokerMessageBytes = buffer.toByteArray();
 
-    BytesMessage message = activeSession.createBytesMessage();
-    message.writeBytes(brokerMessageBytes);
-    MessageProducer producer = activeSession.createProducer(topic);
-    producer.send(message);
-    LOG.info("Message %s | %s send.".formatted(brokerMessage.id(), brokerMessage.value()));
-
-    buffer.close();
-    oos.close();
-  }
+            BytesMessage message = activeSession.createBytesMessage();
+            message.writeBytes(brokerMessageBytes);
+            MessageProducer producer = activeSession.createProducer(topic);
+            producer.send(message);
+            LOG.info("Message %s | %s send.".formatted(brokerMessage.id(), brokerMessage.value()));
+        }
+    }
 }
